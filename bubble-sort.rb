19983b120ee60c634b64a73a28eae11ff31d4666
khantuevao <khantuevao@gmail.com>
2023-11-27 19:22:01 +0600

    Update solution to the problem

--- bubble-sort.rb
+++ bubble-sort.rb
@@ -1,22 +1,21 @@
 # build a method that takes an array and returns a sorted array
 def bubble_sort(array)
+  # introduce a variable for storage when swapping values
   temp = 0
-  count = 0
-
-  unless count == array.length - 1
+  # repeat sorting depending on the array length
+  (array.length - 1).times do
     array.each_with_index do |num, index|
-        # if it is the last num, skip
-        if index == array.length - 1
-            next
-        end
-        # compare 2 nums in array
-        if array[index] > array[index + 1]
+      # if it is the last num, skip the iteration
+      if index == array.length - 1
+        next
+      end
+      # compare 2 nums & swap places if the left one is bigger
+      if array[index] > array[index + 1]
         temp = array[index]
         array[index] = array[index + 1]
         array[index + 1] = temp
-        end
+      end
     end
-    count++
   end
   array
 end
